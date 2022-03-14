17fbc70063be58744363ad6b282b86b3265f7da5
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2022-03-14 12:31:21 +0000

    Pressing buttons logs their button text on the display

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', init());
 
-function handleClick() {
-    console.log('clicked');
-}
-
 function init() {
     let buttons = document.querySelectorAll('.button');
     buttons.forEach(button => {
         button.addEventListener('click', handleClick);
     });
+}
+
+function handleClick() {
+    let display = document.querySelector('.display');
+    let displayText = display.innerText;
+    let textAddition = this.innerText;
+    displayText = displayText + " " + textAddition;
+    display.innerText = displayText;
 }
