7b54c0b41aa272c268a4b80a7fb758afb0c9862d
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2022-03-14 12:53:03 +0000

    Only numbers appear whe clicked

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,28 @@
 document.addEventListener('DOMContentLoaded', init());
 
 function init() {
-    let buttons = document.querySelectorAll('.button');
-    buttons.forEach(button => {
-        button.addEventListener('click', handleClick);
+    let numberButtons = document.querySelectorAll('.button.number');
+    numberButtons.forEach(numberButton => {
+        numberButton.addEventListener('click', handleNumberButtonClick);
+    });
+    let operatorButtons = document.querySelectorAll('.button.operator');
+    operatorButtons.forEach(operatorButton => {
+        operatorButton.addEventListener('click', handleOperatorButtonClick);
     });
 }
 
-function handleClick() {
+function handleNumberButtonClick() {
     let textAddition = this.innerText;
-    updateDisplay(textAddition);
+    addToDisplay(textAddition);
 }
 
-function updateDisplay(textAddition) {
+function handleOperatorButtonClick() {
+    console.log('operator clicked');
+}
+
+function addToDisplay(textAddition) {
     let display = document.querySelector('.display');
     let displayText = display.innerText;
-    displayText = displayText + " " + textAddition;
+    displayText = displayText + textAddition;
     display.innerText = displayText;
 }
